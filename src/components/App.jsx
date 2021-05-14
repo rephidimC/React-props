c260d1e279b63193a9537a2abd1bf45acc2a3641
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-14 17:27:21 +0100

    modified props to allow mapping technique

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
-import Avatar from "./Avatar";
+
+function createCard(contacts) {
+  return (
+    <Card
+      id={contacts.id}
+      //you might be thinking that using key=contacts.id should be enough, but react is a system that uses the key to simply identify the various card elements; hence, the developer cannot really tap into it.
+      key={contacts.id}
+      name={contacts.name}
+      src={contacts.imgURL}
+      tel={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         src={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +38,7 @@ function App() {
         src={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
